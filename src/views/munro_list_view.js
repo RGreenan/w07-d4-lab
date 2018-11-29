520e4264dfccd4cfc0010f9ef840038d31b244b8
Alex Clements <alexfjclements@gmail.com>
2018-11-29 13:30:12 +0000

    munro list view complete

--- src/views/munro_list_view.js
+++ src/views/munro_list_view.js
@@ -1,7 +1,22 @@
 const PubSub = require('../helpers/pub_sub.js');
+const MunroView = require('./munro_view.js')
 
-const MunroListView = function () {
+const MunroListView = function (container) {
+  this.container = container;
+};
 
+MunroListView.prototype.bindEvents = function () {
+  PubSub.subscribe('Munros:munro-list-ready', (event) => {
+    this.munros = event.detail;
+    this.render;
+  });
+};
+
+MunroListView.prototype.render = function () {
+  this.munros.forEach((munro) => {
+    const munroView = new MunroView(this.container, munro);
+    munroView.render();
+  })
 };
 
 module.exports = MunroListView;
