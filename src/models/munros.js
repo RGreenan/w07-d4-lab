b0ee0ee6e498c9ef9dd52877b831992c3b723386
Alex Clements <alexfjclements@gmail.com>
2018-11-29 12:30:04 +0000

    model initial code written

--- src/models/munros.js
+++ src/models/munros.js
@@ -1,8 +1,17 @@
 const PubSub = require('../helpers/pub_sub.js');
-const Request = require('../helpers/request_helper.js');
+const RequestHelper = require('../helpers/request_helper.js');
 
 const Munros = function () {
+  this.data = null;
+};
 
+Munros.prototype.getData = function () {
+  const requestHelper = new RequestHelper('https://munroapi.herokuapp.com/api/munros');
+  requestHelper.get()
+  .then((munros) => {
+    this.data = munros;
+    PubSub.publish('Munros:munro-list-ready', this.data);
+  });
 };
 
 module.exports = Munros;
